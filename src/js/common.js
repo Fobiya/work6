b6de7abb225dcbab6e54d133aeba827b4e9f9555
Sebastian Wolff <sebastian.wolff@albabusinessgroup.se>
2022-07-25 18:52:47 +0300

    25.07.2022

--- src/js/common.js
+++ src/js/common.js
@@ -6,161 +6,161 @@ define(["jquery"], function($) {
 
   
         
-
-//show and hide topbtn
-function yScroll() {
-
-  var yPos = window.pageYOffset;
-
-  if (yPos >= window.innerHeight - 10) {
-    document.getElementById("topbtn").style.opacity = 1;
-  } else {
-    document.getElementById("topbtn").style.opacity = 0;
-  }
-}
-
-//scroll to top
-function goTop () {
-  let topbtn = document.getElementById('topbtn');
-  let top = window.pageYOffset;
-
-  var intervalTimer = setInterval(function() {
-    if (top > 0) {
-      top -= 15;
-      window.scrollTo(0, top);
-    } else {
-      topbtn.style.opacity = 0;
-      clearInterval(intervalTimer);
-    }
-  }, 0.5);
-}
-
-function showAnimate(arrivePoint) {
-  let flag = true;
-  let timer = setInterval(function() {
-
-    let icur = window.pageYOffset;
-    //Buffer movement, speed change at any time
-    let speed = (arrivePoint - icur) / 12;
-    speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);//Take integer speed
-    if (icur || icur == 0) {
-      window.scrollTo(0, icur + speed);
-    }
-    if (arrivePoint !== icur) {
-      flag = false;
-    } else {
-      flag = true;
-    }
-
-    if (flag) {
-      clearInterval(timer);
-      isComplete = true;
-    }
-  },13);
-}
-
-function titleShow() {
-  let Ospan = document.getElementsByTagName("div");
-  let p = window.pageYOffset + window.innerHeight;
-  let q = window.pageYOffset + window.innerHeight*2;
-  for (let i = 0; i<Ospan.length; i++)
-  {
-    if (p < Ospan[i].offsetTop && Ospan[i].offsetTop < q) {
-      Ospan[i].style.opacity = 1;
-    }
-  }
-}
-
-var isComplete = true;
-
-function wheel (e){
-
-  if(isComplete == true){ //Prevent multiple repeat function
-
-    isComplete = false;
-
-    if (e.wheelDelta) {  //IE,Chrome MouseScroll
-      stopScroll();
-      if (e.wheelDelta > 0) { //When the pulley rolls up
-        let arrivePoint = window.pageYOffset - window.innerHeight;
-        //arrivePoint cannot be negative
-        arrivePoint = arrivePoint < 0 ? 0 :arrivePoint;
-        showAnimate(arrivePoint);
-        }
-
-        if (e.wheelDelta < 0) { //When the pulley rolls down
-          let arrivePoint = window.pageYOffset + window.innerHeight;
-          //maximum rolling point
-          let maxBottom = document.body.offsetHeight - window.innerHeight;
-          //If arrivePoint exceeds the maximum rolling point, then arrivePoint equals the maximum rolling point
-          arrivePoint = arrivePoint > maxBottom ? maxBottom : arrivePoint;
-          showAnimate(arrivePoint);
-          titleShow();
-        }
-      }
-      else if (e.detail) {  //Firefox MouseScroll
-      if (e.detail< 0) { //When the pulley rolls up
-        let arrivePoint = document.documentElement.scrollTop - window.innerHeight;
-        //arrivePoint cannot be negative
-        arrivePoint = arrivePoint < 0 ? 0 :arrivePoint;
-        showAnimate(arrivePoint);
-      }
-      if (e.detail> 0) { //When the pulley rolls down
-        let arrivePoint = document.documentElement.scrollTop + window.innerHeight;
-        let maxBottom = document.body.offsetHeight - window.innerHeight;
-        //If arrivePoint exceeds the maximum rolling point, then arrivePoint equals the maximum rolling point
-        arrivePoint = arrivePoint > maxBottom ? maxBottom : arrivePoint;
-        showAnimate(arrivePoint);
-        titleShow();
-      }
-    }
-  }
-}
-
-//Bubble prevention
-function stopScroll() {
-  var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
-
-  function preventDefault(e) {
-    e = e || window.event;
-    if (e.preventDefault)
-      e.preventDefault();
-    e.returnValue = false;
-  }
-
-  function preventDefaultForScrollKeys(e) {
-    if (keys[e.keyCode]) {
-      preventDefault(e);
-      return false;
-    }
-  }
-  var oldonwheel, oldonmousewheel1, oldonmousewheel2, oldontouchmove, oldonkeydown
-      , isDisabled;
-  (function disableScroll() {
-    if (window.addEventListener) // older FF
-    window.addEventListener('DOMMouseScroll', preventDefault, false);
-    oldonwheel = window.onwheel;
-    window.onwheel = preventDefault; // modern standard
-
-    oldonmousewheel1 = window.onmousewheel;
-    window.onmousewheel = preventDefault; // older browsers, IE
-    oldonmousewheel2 = document.onmousewheel;
-    document.onmousewheel = preventDefault; // older browsers, IE
-
-    oldontouchmove = window.ontouchmove;
-    window.ontouchmove = preventDefault; // mobile
-
-    oldonkeydown = document.onkeydown;
-    document.onkeydown = preventDefaultForScrollKeys;
-    isDisabled = true;
-  })();
-}
-
-document.addEventListener("scroll", yScroll);  //control topbtn'opacity
-document.addEventListener('DOMMouseScroll', wheel, false); //firefox
-document.addEventListener('mousewheel', wheel, false); //chrome, IE
-  
-  
+//
+////show and hide topbtn
+//function yScroll() {
+//
+//  var yPos = window.pageYOffset;
+//
+//  if (yPos >= window.innerHeight - 10) {
+//    document.getElementById("topbtn").style.opacity = 1;
+//  } else {
+//    document.getElementById("topbtn").style.opacity = 0;
+//  }
+//}
+//
+////scroll to top
+//function goTop () {
+//  let topbtn = document.getElementById('topbtn');
+//  let top = window.pageYOffset;
+//
+//  var intervalTimer = setInterval(function() {
+//    if (top > 0) {
+//      top -= 15;
+//      window.scrollTo(0, top);
+//    } else {
+//      topbtn.style.opacity = 0;
+//      clearInterval(intervalTimer);
+//    }
+//  }, 0.5);
+//}
+//
+//function showAnimate(arrivePoint) {
+//  let flag = true;
+//  let timer = setInterval(function() {
+//
+//    let icur = window.pageYOffset;
+//    //Buffer movement, speed change at any time
+//    let speed = (arrivePoint - icur) / 12;
+//    speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);//Take integer speed
+//    if (icur || icur == 0) {
+//      window.scrollTo(0, icur + speed);
+//    }
+//    if (arrivePoint !== icur) {
+//      flag = false;
+//    } else {
+//      flag = true;
+//    }
+//
+//    if (flag) {
+//      clearInterval(timer);
+//      isComplete = true;
+//    }
+//  },13);
+//}
+//
+//function titleShow() {
+//  let Ospan = document.getElementsByTagName("div");
+//  let p = window.pageYOffset + window.innerHeight;
+//  let q = window.pageYOffset + window.innerHeight*2;
+//  for (let i = 0; i<Ospan.length; i++)
+//  {
+//    if (p < Ospan[i].offsetTop && Ospan[i].offsetTop < q) {
+//      Ospan[i].style.opacity = 1;
+//    }
+//  }
+//}
+//
+//var isComplete = true;
+//
+//function wheel (e){
+//
+//  if(isComplete == true){ //Prevent multiple repeat function
+//
+//    isComplete = false;
+//
+//    if (e.wheelDelta) {  //IE,Chrome MouseScroll
+//      stopScroll();
+//      if (e.wheelDelta > 0) { //When the pulley rolls up
+//        let arrivePoint = window.pageYOffset - window.innerHeight;
+//        //arrivePoint cannot be negative
+//        arrivePoint = arrivePoint < 0 ? 0 :arrivePoint;
+//        showAnimate(arrivePoint);
+//        }
+//
+//        if (e.wheelDelta < 0) { //When the pulley rolls down
+//          let arrivePoint = window.pageYOffset + window.innerHeight;
+//          //maximum rolling point
+//          let maxBottom = document.body.offsetHeight - window.innerHeight;
+//          //If arrivePoint exceeds the maximum rolling point, then arrivePoint equals the maximum rolling point
+//          arrivePoint = arrivePoint > maxBottom ? maxBottom : arrivePoint;
+//          showAnimate(arrivePoint);
+//          titleShow();
+//        }
+//      }
+//      else if (e.detail) {  //Firefox MouseScroll
+//      if (e.detail< 0) { //When the pulley rolls up
+//        let arrivePoint = document.documentElement.scrollTop - window.innerHeight;
+//        //arrivePoint cannot be negative
+//        arrivePoint = arrivePoint < 0 ? 0 :arrivePoint;
+//        showAnimate(arrivePoint);
+//      }
+//      if (e.detail> 0) { //When the pulley rolls down
+//        let arrivePoint = document.documentElement.scrollTop + window.innerHeight;
+//        let maxBottom = document.body.offsetHeight - window.innerHeight;
+//        //If arrivePoint exceeds the maximum rolling point, then arrivePoint equals the maximum rolling point
+//        arrivePoint = arrivePoint > maxBottom ? maxBottom : arrivePoint;
+//        showAnimate(arrivePoint);
+//        titleShow();
+//      }
+//    }
+//  }
+//}
+//
+////Bubble prevention
+//function stopScroll() {
+//  var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+//
+//  function preventDefault(e) {
+//    e = e || window.event;
+//    if (e.preventDefault)
+//      e.preventDefault();
+//    e.returnValue = false;
+//  }
+//
+//  function preventDefaultForScrollKeys(e) {
+//    if (keys[e.keyCode]) {
+//      preventDefault(e);
+//      return false;
+//    }
+//  }
+//  var oldonwheel, oldonmousewheel1, oldonmousewheel2, oldontouchmove, oldonkeydown
+//      , isDisabled;
+//  (function disableScroll() {
+//    if (window.addEventListener) // older FF
+//    window.addEventListener('DOMMouseScroll', preventDefault, false);
+//    oldonwheel = window.onwheel;
+//    window.onwheel = preventDefault; // modern standard
+//
+//    oldonmousewheel1 = window.onmousewheel;
+//    window.onmousewheel = preventDefault; // older browsers, IE
+//    oldonmousewheel2 = document.onmousewheel;
+//    document.onmousewheel = preventDefault; // older browsers, IE
+//
+//    oldontouchmove = window.ontouchmove;
+//    window.ontouchmove = preventDefault; // mobile
+//
+//    oldonkeydown = document.onkeydown;
+//    document.onkeydown = preventDefaultForScrollKeys;
+//    isDisabled = true;
+//  })();
+//}
+//
+//document.addEventListener("scroll", yScroll);  //control topbtn'opacity
+//document.addEventListener('DOMMouseScroll', wheel, false); //firefox
+//document.addEventListener('mousewheel', wheel, false); //chrome, IE
+//  
+//  
 
   
   document.addEventListener('DOMContentLoaded', function() {
@@ -940,125 +940,168 @@ for(let div of document.querySelectorAll('footer')){
 // =========================================================== 
 // SLICK SLIDER FULL NAVIGATION
 // ===========================================================    
-
-
-  
-    $('.home__4 .next__').click(function() { 
-    $slider__4.slick('slickNext');
-  });
- 
-  $('.home__4 .prev__').click(function() {
-    $slider__4.slick('slickPrev');
-  });
-    
-    
-  var $slider__4 = $('.slider__4');
   
   
-
-    $slider__4.slick({
-        fade: true,
-        cssEase: 'linear',
-      
-        arrows: false,
-        dots: false,
-      
-        autoplay: false,
-        speed: 1000,
-      
-        infinite: true,
-      
-        adaptiveHeight: true,
-      
-        slidesToShow: 1,
-        slidesToScroll: 1,
-//        responsive: [{
-//                breakpoint: 1500,
-//                settings: {
-//                    arrows: true,
-//                    slidesToShow: 4,
-//                    slidesToScroll: 1,
-//                    infinite: true,
-//                }
-//            },
-//                     {
-//                breakpoint: 1300,
-//                settings: {
-//                    arrows: true,
-//                    slidesToShow: 3,
-//                    slidesToScroll: 1,
-//                    infinite: true,
-//                }
-//            },
-//            {
-//                breakpoint: 1000,
-//                settings: {
-//                    arrows: true,
-//                    slidesToShow: 2,
-//                    slidesToScroll: 1,
-//                    infinite: true,
-//                }
-//            },
-//            {
-//                breakpoint: 430,
-//                settings: {
-//                    arrows: true,
-//                    slidesToShow: 1,
-//                    slidesToScroll: 1,
-//                    infinite: true,
-//                }
-//            }
-//        ]
-    });
-
-
-  
-  
-  
-$('.flat.section__9 .left__').click(function() { 
-  $gallery.slick('slickNext');
+$('.next__arrow').click(function() { 
+  $full__tuch.slick('slickNext');
 });
 
-$('.flat.section__9 .right__').click(function() {
-  $gallery.slick('slickPrev');
-});
+//$('.flat.section__9 .right__').click(function() {
+//  $gallery.slick('slickPrev');
+//});
+  
 
   
-var $gallery = $('.flat.section__9 .box__slider__8');
+    var $full__tuch = $('#full__tuch');
+  
+  
+$full__tuch.slick({
+  arrows: false,
+  dots: false,
+  infinite: false,
 
+  vertical: true,
+  verticalSwiping: true,
+  
+  swipe: true,
 
-  $gallery.slick({
-    arrows: false,
-    speed: 550,
-    fade: true,
-    cssEase: 'linear',
-    swipe: true,
-    swipeToSlide: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  
+  speed: 500,
+//    swipe: true,
+//    swipeToSlide: true,
     touchThreshold: 10
-  }); 
-
-
-var slideCount = null;
-
-$gallery.on('init', function(event, slick){
-  slideCount = slick.slideCount;
-  setSlideCount();
-  setCurrentSlideNumber(slick.currentSlide);
+  
 });
+  
+  
+  
+  $full__tuch.on('wheel', (function(e) {
+    e.preventDefault();
+    if (e.originalEvent.deltaY < 0) {
+        $(this).slick('slickNext');
+    } else {
+        $(this).slick('slickPrev');
+    }
+ }));
+  
+//    $('.home__4 .next__').click(function() { 
+//    $slider__4.slick('slickNext');
+//  });
+// 
+//  $('.home__4 .prev__').click(function() {
+//    $slider__4.slick('slickPrev');
+//  });
+//    
+//    
+//  var $slider__4 = $('.slider__4');
+//  
+//  
+//
+//    $slider__4.slick({
+//        fade: true,
+//        cssEase: 'linear',
+//      
+//        arrows: false,
+//        dots: false,
+//      
+//        autoplay: false,
+//        speed: 1000,
+//      
+//        infinite: true,
+//      
+//        adaptiveHeight: true,
+//      
+//        slidesToShow: 1,
+//        slidesToScroll: 1,
+////        responsive: [{
+////                breakpoint: 1500,
+////                settings: {
+////                    arrows: true,
+////                    slidesToShow: 4,
+////                    slidesToScroll: 1,
+////                    infinite: true,
+////                }
+////            },
+////                     {
+////                breakpoint: 1300,
+////                settings: {
+////                    arrows: true,
+////                    slidesToShow: 3,
+////                    slidesToScroll: 1,
+////                    infinite: true,
+////                }
+////            },
+////            {
+////                breakpoint: 1000,
+////                settings: {
+////                    arrows: true,
+////                    slidesToShow: 2,
+////                    slidesToScroll: 1,
+////                    infinite: true,
+////                }
+////            },
+////            {
+////                breakpoint: 430,
+////                settings: {
+////                    arrows: true,
+////                    slidesToShow: 1,
+////                    slidesToScroll: 1,
+////                    infinite: true,
+////                }
+////            }
+////        ]
+//    });
 
-$gallery.on('beforeChange', function(event, slick, currentSlide, nextSlide){
-  setCurrentSlideNumber(nextSlide);
-});
 
-function setSlideCount() {
-  var $el = $('.rslide-count-wrap').find('.rtotal');
-  $el.text(slideCount);
-}
-
-function setCurrentSlideNumber(currentSlide) {
-  var $el = $('.rslide-count-wrap').find('.rcurrent');
-  $el.text(currentSlide + 1);
-} 
+  
+  
+  
+//$('.flat.section__9 .left__').click(function() { 
+//  $gallery.slick('slickNext');
+//});
+//
+//$('.flat.section__9 .right__').click(function() {
+//  $gallery.slick('slickPrev');
+//});
+//
+//  
+//var $gallery = $('.flat.section__9 .box__slider__8');
+//
+//
+//  $gallery.slick({
+//    arrows: false,
+//    speed: 550,
+//    fade: true,
+//    cssEase: 'linear',
+//    swipe: true,
+//    swipeToSlide: true,
+//    touchThreshold: 10
+//  }); 
+//
+//
+//var slideCount = null;
+//
+//$gallery.on('init', function(event, slick){
+//  slideCount = slick.slideCount;
+//  setSlideCount();
+//  setCurrentSlideNumber(slick.currentSlide);
+//});
+//
+//$gallery.on('beforeChange', function(event, slick, currentSlide, nextSlide){
+//  setCurrentSlideNumber(nextSlide);
+//});
+//
+//function setSlideCount() {
+//  var $el = $('.rslide-count-wrap').find('.rtotal');
+//  $el.text(slideCount);
+//}
+//
+//function setCurrentSlideNumber(currentSlide) {
+//  var $el = $('.rslide-count-wrap').find('.rcurrent');
+//  $el.text(currentSlide + 1);
+//} 
   
 
 
